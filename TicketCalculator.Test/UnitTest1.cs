377338b9636e81042e4471b70f7c9322433c6272
Jeremy Thiriet <jeremy-externe.thiriet@erdf.fr>
2016-04-08 13:42:59 +0200

    add test project

---
+++ TicketCalculator.Test/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Windows.Input;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TicketCalculator.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var vm = new MainPageViewModel();
+            ICommand c = vm.ComputeCommand;
+
+
+        }
+    }
+}
