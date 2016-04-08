34a538db36d830b968a81bbe8a918914741132c3
Jeremy Thiriet <jeremy-externe.thiriet@erdf.fr>
2016-04-08 14:04:07 +0200

    add android icons

---
+++ TicketCalculator/TicketCalculator.Droid/resize.bat
@@ -0,0 +1,6 @@
+REM http://www.imagemagick.org/script/binary-releases.php
+
+convert -size 72 Icon.svg Resources/drawable/icon.png
+convert -size 72 Icon.svg Resources/drawable-hdpi/icon.png
+convert -size 96 Icon.svg Resources/drawable-xhdpi/icon.png
+convert -size 144 Icon.svg Resources/drawable-xxhdpi/icon.png
